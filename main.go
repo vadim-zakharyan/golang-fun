816fa119e0c5717619709f3521de0c41b70b879c
Vadim Zakharyan <vadim.zakharyan@mgnnetworks.com>
2021-08-05 17:16:21 +0500

    feat: print from structs result

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	numberList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	PrintOddEvenSum(numberList)
+	oddEvenSum := OddEvenSum{}
+	display(oddEvenSum.Run([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}))
+
+	fibonacci := Fibonacci{}
+	display(fibonacci.Run(35))
+
+}
+
+func display(value string) {
+	fmt.Printf("%s\n\n", value)
 }
